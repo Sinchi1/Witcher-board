6499869971b121689d6bc309d553b09b31eb003d
Sinchi <133340035+Sinchi1@users.noreply.github.com>
2026-01-10 14:37:28 +0300

    Auth service init

---
+++ authService/src/main/java/org/kursach/AuthMain.java
@@ -0,0 +1,11 @@
+package org.kursach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthMain {
+    public static void main(String... args ){
+        SpringApplication.run(AuthMain.class, args);
+    }
+}
